f2cbf77d7f92d0e6bb05aa6c22be6a8daf1973f5
tomhughes87 <tomhughes1987@gmail.com>
2022-05-11 18:35:56 +0200

    front-back talking together in practice files 'fakeSearchResults'

--- fakeSearchResults.js
+++ fakeSearchResults.js
@@ -1,5 +1,11 @@
 const blankDiv = document.querySelector("div");
+const apiPlace = document.getElementById("placeholderForAPI");
+
+apiPlace.textContent = "tessssssssssssting";
 
 fetch("http://localhost:3000/search/1")
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    console.log(data);
+    apiPlace.textContent = data.descriptions;
+  });
