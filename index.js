676ffe41254ba009912d1f5d22df10521cf1e31c
tomhughes87 <tomhughes1987@gmail.com>
2022-05-11 17:20:50 +0200

    add darkmode toggle

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const about = document.getElementById("aboutLink");
 const darkmode = document.getElementById("darkLink");
+let CheckDarkOn = false;
 
 ///////ABOUT/////////
 about.addEventListener("click", openAbout);
@@ -13,19 +14,22 @@ function openAbout() {
 
 ///////darkmode/////////
 darkmode.addEventListener("click", swicthColours);
-const darkOn = false;
 
 function swicthColours(e) {
+  console.log("dark mode clicked");
+  console.log(CheckDarkOn);
   e.preventDefault();
   let mainbody = document.querySelector("body");
+  //   mainbody.style.backgroundColor = "rgb(39, 39, 39)";
+  //   mainbody.style.color = "white";
 
-  if ((darkOn = false)) {
+  if (CheckDarkOn === false) {
     mainbody.style.backgroundColor = "rgb(39, 39, 39)";
     mainbody.style.color = "white";
-    let darkOn = true;
+    CheckDarkOn = true;
   } else {
     mainbody.style.backgroundColor = "white";
     mainbody.style.color = "black";
-    let darkOn = true;
+    CheckDarkOn = false;
   }
 }
